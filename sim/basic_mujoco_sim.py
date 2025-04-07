f3ba30675dbb2b3f56afe3c4afdc22dbdb3fced0
kimsooyoung <tge1375@naver.com>
2025-04-07 13:31:02 -0700

    basic mujoco view added

--- sim/basic_mujoco_sim.py
+++ sim/basic_mujoco_sim.py
@@ -2,16 +2,27 @@ import mujoco
 import mujoco_viewer
 
 ROBOT_SCENE = "./unitree_a1/scene.xml" # Robot scene
-ROBOT_SCENE = "./unitree_go1/scene_torque.xml" # Robot scene
+# ROBOT_SCENE = "./unitree_go1/scene_position.xml" # Robot scene
+# ROBOT_SCENE = "./unitree_go1/scene_torque.xml" # Robot scene
 
-mj_model = mujoco.MjModel.from_xml_path(ROBOT_SCENE)
-mj_data = mujoco.MjData(mj_model)
+model = mujoco.MjModel.from_xml_path(ROBOT_SCENE)
+data = mujoco.MjData(model)
 
-viewer = mujoco_viewer.MujocoViewer(mj_model, mj_data)
+viewer = mujoco_viewer.MujocoViewer(model, data)
+mujoco.mj_resetDataKeyframe(model, data, 0)
 
 # step at least once to load model in viewer
-mujoco.mj_step(mj_model, mj_data)  
+mujoco.mj_step(model, data)  
 
-while viewer.is_alive:
-    viewer.render()
+for i in range(1000):
+    while viewer.is_alive:
+        mujoco.mj_step(model, data)
+        mujoco.mj_rnePostConstraint(model, data)
+        mujoco.mj_fwdActuation(model, data)
+
+        data.ctrl[:] = model.key_ctrl
+
+        viewer.render()
+    else:
+        break
 viewer.close()
